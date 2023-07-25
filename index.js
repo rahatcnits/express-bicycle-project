a9bde1e8484996248e04a856a85e423730d051be
rahatcnits <rahatcnits@gmail.com>
2023-07-25 12:13:46 +0600

    second commit

--- index.js
+++ index.js
@@ -3,13 +3,22 @@ const app = express();
 
 const bicycles = require("./data/data.json");
 
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
-  return res.send(bicycles);
+  return res.render("bicycles", {
+    bicycles,
+  });
 });
 
 app.get("/bicycle", (req, res) => {
   const bicycle = bicycles.find((b) => b.id === req.query.id);
-  return res.send(bicycle);
+  console.log(bicycle);
+
+  return res.render("overview", {
+    bicycle,
+  });
 });
 
 app.listen(3000, () => console.log("Server is runing at port 3000"));
